03b13932ccae43ef79fe151ac45b2842524d5662
waseembaloch21 <waseemrauf672@gmail.com>
2026-02-15 19:28:19 +0500

    code updated

--- my-practice-app/src/components/TodoApp.vue
+++ my-practice-app/src/components/TodoApp.vue
@@ -1,14 +1,19 @@
 <script setup>
 import { ref } from 'vue';
 
-const input = ref('')
-const todos = ref([])
+const emoji = ref("😍")
+const status_code = ref("200")
+const status_message = ref("Server is running")
+const button_text = ref("good bye")
 
-const addTodo = () => {
- if (input.value.trim() !== '') {
-    todos.value.push(input.value)
-    input.value = ''
-}}
+// const input = ref('')
+// const todos = ref([])
+
+// const addTodo = () => {
+//  if (input.value.trim() !== '') {
+//     todos.value.push(input.value)
+//     input.value = ''
+// }}
 
 // const addTodoEnter =(event) => {
 //      if (event.keyCode === 10) {
@@ -18,20 +23,28 @@ const addTodo = () => {
     
 // }
 
-const deleteTodo = (event) => {
-      event.target.parentNode.remove();  
-}
+// const deleteTodo = (event) => {
+//       event.target.parentNode.remove();  
+// }
 
 </script>
 
 <template>
-    <input v-model="input" class=" ml-10 mt-10 border-2" type="text" placeholder="enter a Todo">
+
+    <span>{{ emoji }}</span>
+    <h1>{{ status_code }}</h1>
+    <p>{{ status_message }}</p>
+    <a href="#" target="_blank">{{ button_text }} →</a>
+
+
+
+    <!-- <input v-model="input" class=" ml-10 mt-10 border-2" type="text" placeholder="enter a Todo">
     <button @click="addTodo"
     class=" ml-1 border-2 bg-amber-400">submit</button>
 
     <ul>
         <li class=" ml-5" v-for="(todo , index) in todos" :key="index">{{ todo }} <button @click="deleteTodo" class=" ml-1 border-2 bg-red-400 mb-1.5 mt-1" >delete</button></li>
-    </ul>
+    </ul> -->
 
 </template>
 
