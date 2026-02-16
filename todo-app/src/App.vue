76a8752a9a1b5e3a8ce467348e60d0ed0b5cff28
waseembaloch21 <waseemrauf672@gmail.com>
2026-02-16 23:34:21 +0500

    code updated

--- todo-app/src/App.vue
+++ todo-app/src/App.vue
@@ -1,23 +1,14 @@
 <script setup>
 import HelloWorld from './components/HelloWorld.vue'
+import Login from './components/Login.vue';
 </script>
 
 <template>
- 
-  <HelloWorld/>
+  
+  <Login/>
+  
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+
 </style>
