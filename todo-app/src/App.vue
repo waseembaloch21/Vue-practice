0968a92ce0f5f62ead513bfd05653debf7965c6e
waseembaloch21 <waseemrauf672@gmail.com>
2026-02-17 17:33:00 +0500

    code updated

--- todo-app/src/App.vue
+++ todo-app/src/App.vue
@@ -2,13 +2,13 @@
 import HelloWorld from './components/HelloWorld.vue'
 import Login from './components/Login.vue';
 import Signup from './components/Signup.vue';
-import router from './router';
+
+
 </script>
 
 <template>
-    
-<router/>
-  
+    <h1>Hello World</h1>
+    <signup/>
 </template>
 
 <style scoped>
--- todo-app/src/components/Signup.vue
+++ todo-app/src/components/Signup.vue
@@ -1,10 +1,7 @@
 <script setup>
 import { RouterLink } from 'vue-router';
-
 </script>
-
 <template>
-
     <section class="flex flex-col items-center pt-6">
         <div
             class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
