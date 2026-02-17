253f819657520c015722774621dd38eab0133148
waseembaloch21 <waseemrauf672@gmail.com>
2026-02-17 18:59:42 +0500

    code updated

--- todo-app/src/App.vue
+++ todo-app/src/App.vue
@@ -1,14 +1,9 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import Login from './components/Login.vue';
-import Signup from './components/Signup.vue';
-
 
 </script>
 
 <template>
-    <h1>Hello World</h1>
-    <signup/>
+    <router-view/>
 </template>
 
 <style scoped>
--- todo-app/src/main.js
+++ todo-app/src/main.js
@@ -1,6 +1,6 @@
 import { createApp } from 'vue'
 import './style.css'
-import router from './router'
 import App from './App.vue'
+import router from './router'
 
 createApp(App).use(router).mount('#app')
